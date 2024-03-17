6a9c78f39de086b7f91913070b6d46eb887f36bf
hr080100 <sahrizvi@umich.edu>
2024-03-17 15:03:01 +0530

    Commiting the county population file with the pylint check completed and passed.

--- county_population.py
+++ county_population.py
@@ -7,17 +7,31 @@ for further data analysis.
 # Importing the required libraries.
 import pandas as pd
 
+# Creating a function to read the county population excel file.
 def read_county_pop(file_path = 'data_files/county_pop.xlsx'):
+    """
+    This function reads the county population excel
+    file and return it as a dataframe in its raw form.
+    """
     county_pop_raw = pd.read_excel(file_path)
     return county_pop_raw
 
+# Cleaning the county population dataframe.
 def clean_county_pop(county_pop_data):
+    """
+    This function takes the county population dataframe
+    and cleans it so that it can be used for further
+    processing.
+    """
+    # Creating county and state lists to store the values separately.
     county = []
     state = []
+    # Getting the county and state names form the county column.
     for i in county_pop_data.loc[:, 'County']:
         state.append(i.split(', ')[1])
         county.append((i.split(', ')[0])[1:])
 
+    # Removing the parts of the county names not needed.
     names_delete = ['County', 'Borough', 'Area', 'Census', 'Municipality', 'Region', 'Planning']
     for i in enumerate(county):
         if county[i[0]].split()[-1] in names_delete:
@@ -29,12 +43,18 @@ def clean_county_pop(county_pop_data):
         if county[i[0]].split()[-1] in names_delete:
             county[i[0]] = " ".join(county[i[0]].split()[:-1])
 
+    # Adding the colunms of county and state names in the original database.
     county_pop_data['County'] = county
     county_pop_data['State'] = state
 
     return county_pop_data
 
+# Calling the created functions to get the final dataframe.
 def county_pop(file_path = 'data_files/county_pop.xlsx'):
+    """
+    This function calls both the earlier functions and
+    returns the final processed dataframe.
+    """
     county_pop_raw = read_county_pop(file_path)
     county_pop_final = clean_county_pop(county_pop_raw)
     return county_pop_final
